9211485100466c505171733331c8422a7936cc64
andriusandriusandrius <and.zurba@gmail.com>
2025-08-28 18:51:07 +0300

    add hashing

---
+++ backend/Services/PasswordHasher.cs
@@ -0,0 +1,33 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+public static class PasswordHasher
+{
+    public static string Hash(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+        string hashedPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8
+        ));
+        return $"{Convert.ToBase64String(salt)}:{hashedPassword}";
+    }
+    public static bool Verify(string password, string hashedPassword)
+    {
+        string[] parts = hashedPassword.Split(":");
+        byte[] salt = Convert.FromBase64String(parts[0]);
+        string hash = parts[1];
+        string hashOfInput = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8
+        ));
+        return hash == hashOfInput;
+
+    }
+}
