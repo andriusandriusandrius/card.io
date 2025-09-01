d7947704172d548e37eda68b008631976cf8340d
andriusandriusandrius <and.zurba@gmail.com>
2025-09-01 14:17:00 +0300

    initialize vite+react.ts chrome extension layout

--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, "popup.html"),
+        options: resolve(__dirname, "options.html"),
+        background: resolve(__dirname, "src/service-worker.ts"),
+        content: resolve(__dirname, "src/main.ts"),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+});
